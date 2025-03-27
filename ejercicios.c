ad2cc39b9e5f446eef557eede423dff6931ebce7
ohN0-Viridiana-jaksjs <jesusvicentemejiacalderon@gmail.com>
2025-03-27 14:16:05 -0600

    primer ejercicio 7.c

---
+++ ejercicios.c
@@ -0,0 +1,22 @@
+//primer ejercicio//
+
+//otra versión//
+
+#include<stdio.h>
+
+void main ()
+    {
+        int a,b;
+        
+        printf ("dame un numero: ");
+        scanf ("%i",&a);
+        printf ("dame otro valor: ");
+        scanf ("%i",&b);
+        
+        if (a>b)
+        
+        {
+            printf("a es mayor a b ");
+        }
+
+    }
