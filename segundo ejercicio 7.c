10bbf5f17700dbee3966acdba9c4d6fa01f1b7e2
ohN0-Viridiana-jaksjs <jesusvicentemejiacalderon@gmail.com>
2025-03-27 14:31:44 -0600

    Create segundo ejercicio 7.c

---
+++ segundo ejercicio 7.c
@@ -0,0 +1,24 @@
+//programa que valida si un un numero es par o impar//
+
+#include<stdio.h>
+
+void main ()
+
+    {
+        
+        int num;
+        
+        printf("dame un numero: ");
+        scanf ("%i", &num);
+        
+        if(num % 2 == 0)
+        {
+            printf("el numero es par\n");
+        }
+        else
+        {
+        printf("el numero es impar\n");
+        }
+    }
+    
+    
